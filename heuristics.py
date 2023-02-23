09fb880e4cc50bed2c645fb219a3253a7281f23a
Daniel Mai <danielmai122001@gmail.com>
2023-02-23 15:09:47 -0600

    set up heuristic files

---
+++ heuristics.py
@@ -0,0 +1,19 @@
+def h1(puzzle):
+    """
+    Find most constrained
+    :return:
+    """
+
+
+def h2(puzzle):
+    """
+    Find most constraining
+    :return:
+    """
+
+
+def h3(puzzle):
+    """
+    Hybrid
+    :return:
+    """
