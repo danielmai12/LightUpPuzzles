a7338787470adc25f172cc8f07a00728837a18b8
Daniel Mai <danielmai122001@gmail.com>
2023-02-23 16:22:50 -0600

    Fix name

--- heuristics.py
+++ heuristics.py
@@ -34,16 +34,16 @@ def h2(curr_state: 'List[List[str]]', available_cells: 'List[List[int]]' ):
     cells = []
     max_count = 0
 
-    utils.light_state(current_state)
+    utils.light_state(curr_state)
 
     for cell in available_cells:
-        to_be_lit_up = count_should_be_lit_cells(current_state, cell[0], cell[1])
+        to_be_lit_up = count_should_be_lit_cells(curr_state, cell[0], cell[1])
         if to_be_lit_up > max_count:
             cells = [cell]
             max_count = to_be_lit_up
         elif to_be_lit_up == max_count:
             cells.append(cell)
-    is_map_lit_up_and_clean_map(current_state)
+    is_map_lit_up_and_clean_map(curr_state)
     return cells
 
 
